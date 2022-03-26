191cd0ccae8a5efded608fb596b324afe8351245
Daniel Hampton <dhampton084@gmail.com>
2022-03-26 17:23:46 +0000

    wip: added react-router-dom and spa setup

---
+++ src/components/about.tsx
@@ -0,0 +1,7 @@
+export default function About(): JSX.Element {
+  return (
+    <div className={"grid grid-cols-1 place-items-center h-full"}>
+      <h1 className={"text-4xl"}>Hello Twitch Chat! 👋</h1>
+    </div>
+  );
+}
---
+++ src/components/home.tsx
@@ -0,0 +1,7 @@
+export default function About(): JSX.Element {
+  return (
+    <div className={"grid grid-cols-1 place-items-center h-full"}>
+      <h1 className={"text-4xl"}>🏠 Home</h1>
+    </div>
+  );
+}
